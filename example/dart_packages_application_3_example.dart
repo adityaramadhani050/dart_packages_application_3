c6e64fa9355400cbdaacab5baf08af78fae5b229
adityaramadhani050 <adityaramadhani050@gmail.com>
2022-04-24 15:01:26 +0700

    Init packages

---
+++ example/dart_packages_application_3_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_packages_application_3/dart_packages_application_3.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_packages_application_3.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_packages_application_3;
+
+export 'src/dart_packages_application_3_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
