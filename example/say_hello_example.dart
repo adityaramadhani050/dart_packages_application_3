fa950ac48577b6d4c8a59c4f08b6d6137cbd57a2
adityaramadhani050 <adityaramadhani050@gmail.com>
2022-04-24 15:24:48 +0700

    add exmple how to acces the packages

---
+++ example/say_hello_example.dart
@@ -0,0 +1,8 @@
+import 'package:dart_packages_application_3/say_hello.dart';
+
+void main(List<String> args) {
+  print(greeting('Your Name'));
+
+  SuperVisor superVisor = SuperVisor();
+  Manager manager = Manager();
+}
--- lib/say_hello.dart
+++ lib/say_hello.dart
@@ -1,4 +1,4 @@
 library dart_packages_application_3;
 
 export 'src/say_hello_base.dart';
-export 'src/employee.dart' show SuperVisor, Manager;
+export 'src/employee_base.dart' show SuperVisor, Manager;
